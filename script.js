5bbba7efa0cf215091d18d518d284f87b6486152
Tayse Rosa <tayse_rosa@yahoo.com.br>
2020-06-12 14:06:22 -0300

    Multiplication table developed in the Video Course Javascript

---
+++ script.js
@@ -0,0 +1,23 @@
+function tabuada(){
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+
+    if(num.value.length == 0){
+        window.alert('Por favor digite um numero');
+    }else{
+        let n = Number(num.value)
+        c = 1
+
+        //limpar o select para mostrar nova tabuada
+        tab.innerHTML = ''
+
+        while (c <=10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+    
+}
